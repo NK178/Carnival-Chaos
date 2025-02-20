7488f2a19445e3b95899f13fadf86c99ad5f5c3c
azyriuth <azyriuth@gmail.com>
2025-02-20 16:40:30 +0800

    Yong Quan sln NEW v7.6.1

--- Application/Source/MeshBuilder.cpp
+++ Application/Source/MeshBuilder.cpp
@@ -1,8 +1,8 @@
+#define _USE_MATH_DEFINES
 #include "MeshBuilder.h"
 #include <GL\glew.h>
 #include <vector>
 #include "LoadOBJ.h"
-#define USE_MATHS_DEFINES
 
 /******************************************************************************/
 /*!
