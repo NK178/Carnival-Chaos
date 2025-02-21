c849746d7653a82f51992f33ddb0acb7ab27d050
NK178 <ayhngern@gmail.com>
2025-02-21 13:31:22 +0800

    making scene whacka mole

---
+++ Application/Source/SceneWhackAMole.h
@@ -0,0 +1,155 @@
+#ifndef SCENE_WIU_TEST_H
+#define SCENE_WIU_TEST_H
+
+#include "Scene.h"
+#include "Mesh.h"
+#include "Application.h"
+#include "FPCamera.h"
+#include "MatrixStack.h"
+#include "Light.h"
+#include "CTree.h"
+#include "GameObject.h"
+
+class SceneWhackAMole : public Scene
+{
+public:
+	enum GEOMETRY_TYPE
+	{
+		GEO_AXES,
+		//shapes
+		GEO_CUBE,
+		GEO_QUAD,
+		GEO_SPHERE,
+		GEO_PLANE,
+		GEO_HAMMER1,
+		GEO_HAMMER2,
+		GEO_HAMMER3,
+
+		//Text
+		GEO_TEXT,
+
+		//SKybox
+		GEO_LEFT,
+		GEO_RIGHT,
+		GEO_TOP,
+		GEO_BOTTOM,
+		GEO_FRONT,
+		GEO_BACK,
+		NUM_GEOMETRY,
+	};
+
+	enum UNIFORM_TYPE
+	{
+		U_MVP = 0,
+		U_MODELVIEW,
+		U_MODELVIEW_INVERSE_TRANSPOSE,
+		U_MATERIAL_AMBIENT,
+		U_MATERIAL_DIFFUSE,
+		U_MATERIAL_SPECULAR,
+		U_MATERIAL_SHININESS,
+		/*FLASHLIGHT*/
+		U_LIGHT0_TYPE,
+		U_LIGHT0_POSITION,
+		U_LIGHT0_COLOR,
+		U_LIGHT0_POWER,
+		U_LIGHT0_KC,
+		U_LIGHT0_KL,
+		U_LIGHT0_KQ,
+		U_LIGHT0_SPOTDIRECTION,
+		U_LIGHT0_COSCUTOFF,
+		U_LIGHT0_COSINNER,
+		U_LIGHT0_EXPONENT,
+		/*FLASHLIGHT*/
+		/*MOON*/
+		U_LIGHT1_TYPE,
+		U_LIGHT1_POSITION,
+		U_LIGHT1_COLOR,
+		U_LIGHT1_POWER,
+		U_LIGHT1_KC,
+		U_LIGHT1_KL,
+		U_LIGHT1_KQ,
+		U_LIGHT1_SPOTDIRECTION,
+		U_LIGHT1_COSCUTOFF,
+		U_LIGHT1_COSINNER,
+		U_LIGHT1_EXPONENT,
+		/*MOON*/
+		/*LANTERN*/
+		U_LIGHT2_TYPE,
+		U_LIGHT2_POSITION,
+		U_LIGHT2_COLOR,
+		U_LIGHT2_POWER,
+		U_LIGHT2_KC,
+		U_LIGHT2_KL,
+		U_LIGHT2_KQ,
+		U_LIGHT2_SPOTDIRECTION,
+		U_LIGHT2_COSCUTOFF,
+		U_LIGHT2_COSINNER,
+		U_LIGHT2_EXPONENT,
+		/*LANTERN*/
+		U_NUMLIGHTS,
+		// Step 5
+		U_COLOR_TEXTURE_ENABLED,
+		U_COLOR_TEXTURE,
+
+		U_LIGHTENABLED,
+
+		U_TEXT_ENABLED,
+		U_TEXT_COLOR,
+
+		U_TOTAL,
+	};
+
+	SceneWhackAMole();
+	~SceneWhackAMole();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+
+private:
+	void HandleKeyPress();
+	void RenderMesh(Mesh* mesh, bool enableLight);
+
+	unsigned m_vertexArrayID;
+	Mesh* meshList[NUM_GEOMETRY];
+
+	unsigned m_programID;
+	unsigned m_parameters[U_TOTAL];
+
+	//AltAzCamera camera;
+	FPCamera camera;
+	int projType = 1; // fix to 0 for orthographic, 1 for projection
+
+	MatrixStack modelStack, viewStack, projectionStack;	
+
+	static const int NUM_LIGHTS = 3;
+	Light light[NUM_LIGHTS];
+	bool enableLight;
+
+	Application app;
+	
+	void RenderMeshOnScreen(Mesh* mesh, float x, float y, float sizex, float sizey);
+	void RenderText(Mesh* mesh, std::string text, glm::vec3 color);
+	void RenderTextOnScreen(Mesh* mesh, std::string text, glm::vec3 color, float size, float x, float y);
+	void Material(GEOMETRY_TYPE obj, float AmR, float AmG, float AmB, float DifA, float DifG, float DifB, float SpA, float SpG, float SpB, float Shiny);
+	void RenderSkyBox();
+
+
+	struct Cube : public GameObject {
+		glm::vec3 boxextent{ 1.5f,1.5f,1.5f };
+		Cube(int id, int type) : GameObject(id, type) {}
+	};
+	struct Sphere : public GameObject {
+		float radius;
+		Sphere(int id, int r, int type) : radius(r), GameObject(id, type) {}
+	};
+
+	bool activate = false;
+	std::vector<Cube> cubelist;
+	std::vector<Sphere> spherelist;	
+
+
+};
+
+#endif
