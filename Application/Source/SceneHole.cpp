c4dba6ab814566c4e4ece3d0f21f5b4164902cd4
WR540 <wilsoncheng889@gmail.com>
2025-02-26 13:13:57 +0800

    SceneHole now less cringe

--- Application/Source/SceneHole.cpp
+++ Application/Source/SceneHole.cpp
@@ -32,7 +32,7 @@ void SceneHole::Init()
 	failedGrav = 0;
 
 	camera.enableFNAF = true;
-	camera.allowMovement = false;
+	camera.pos.x -= 2;
 	camera.allowJump = false;
 	camera.allowSprint = false;
 	camera.allowCrouch = false;
@@ -310,14 +310,14 @@ void SceneHole::Update(double dt)
 			{
 				if (camera.pos.z > -12 && camera.pos.y > 2)
 				{
-					camera.allowMovement = false;
-					camera.AddForce(glm::vec3(-200, -10, 0));
+					camera.pos.x -= 2;
+					
 				}
 			}
 			else
 			{
-				camera.allowMovement = false;
-				camera.AddForce(glm::vec3(-200, -10, 0));
+				camera.pos.x -= 2;
+				
 			}
 		}
 
@@ -328,15 +328,15 @@ void SceneHole::Update(double dt)
 			{
 				if (camera.pos.z > -3 && camera.pos.z < 3)
 				{
-					camera.allowMovement = false;
-					camera.AddForce(glm::vec3(-200, -10, 0));
+					camera.pos.x -= 2;
+					
 				}
 				else
 				{
 					if (camera.pos.y < 3.1)
 					{
-						camera.allowMovement = false;
-						camera.AddForce(glm::vec3(-200, -10, 0));
+						camera.pos.x -= 2;
+						
 					}
 				}
 			}
@@ -349,14 +349,14 @@ void SceneHole::Update(double dt)
 			{
 				if (camera.pos.y > 2)
 				{
-					camera.allowMovement = false;
-					camera.AddForce(glm::vec3(-200, -10, 0));
+					camera.pos.x -= 2;
+					
 				}
 			}
 			else
 			{
-				camera.allowMovement = false;
-				camera.AddForce(glm::vec3(-200, -10, 0));
+				camera.pos.x -= 2;
+				
 			}
 		}
 
@@ -365,23 +365,23 @@ void SceneHole::Update(double dt)
 		{
 			if (camera.pos.y == 3)
 			{
-				camera.allowMovement = false;
-				camera.AddForce(glm::vec3(-200, -10, 0));
+				camera.pos.x -= 2;
+				
 			}
 			else if (camera.pos.y > 3.1)
 			{
 				if (camera.pos.z > -8 && camera.pos.z < 8)
 				{
-					camera.allowMovement = false;
-					camera.AddForce(glm::vec3(-200, -10, 0));
+					camera.pos.x -= 2;
+					
 				}
 			}
 			else
 			{
 				if ((camera.pos.z < -8 || camera.pos.z > 8))
 				{
-					camera.allowMovement = false;
-					camera.AddForce(glm::vec3(-200, -10, 0));
+					camera.pos.x -= 2;
+					
 				}
 			}
 		}
@@ -391,15 +391,15 @@ void SceneHole::Update(double dt)
 		{
 			if (camera.pos.y < 3.1)
 			{
-				camera.allowMovement = false;
-				camera.AddForce(glm::vec3(-200, -10, 0));
+				camera.pos.x -= 2;
+				
 			}
 			else
 			{
 				if (camera.pos.z > -6 || camera.pos.z < -17)
 				{
-					camera.allowMovement = false;
-					camera.AddForce(glm::vec3(-200, -10, 0));
+					camera.pos.x -= 2;
+					
 				}
 			}
 		}
