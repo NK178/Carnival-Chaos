33e2a1cc1a6162b1f7200c5249a351430032f7eb
NK178 <ayhngern@gmail.com>
2025-02-27 21:25:21 +0800

    Set first scene as main menu

--- Application/Source/Application.cpp
+++ Application/Source/Application.cpp
@@ -150,7 +150,7 @@ void Application::Init()
 		glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
 	// Initialize scene manager with main menu scene
-	sceneManager.Init(SCENE_CARNIVAL);
+	sceneManager.Init(SCENE_MAINMENU);
 }
 
 void Application::Run()
